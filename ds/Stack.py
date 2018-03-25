b80704462d10863e60e22e89f04ec5fda63f516b
Sanshit Sharma <sansshar@cisco.com>
2018-03-24 22:34:11 -0700

    Added empty check to pop

--- ds/Stack.py
+++ ds/Stack.py
@@ -4,17 +4,23 @@ class Stack:
 	def __init__(self):
 		self.items = []
 
-	def isEmpty(self):
-		return self.items == []
-
 	def push(self, item):
 		self.items.append(item)
 
 	def pop(self):
-		return self.items.pop()
+		if not self.isEmpty():
+			return self.items.pop()		
+		return None
 
 	def peek(self):
-		return self.items[len(self.items)-1]
+		if not self.isEmpty():
+			return self.items[len(self.items)-1]
+		return None
 
 	def size(self):
 		return len(self.items)
+
+        @property
+	def isEmpty(self):
+		return self.items == []
+
